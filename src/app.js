1d2b24da1f6d851ed2ba84d2b685a6d91cce6b05
Eric Quy Nguyen <nguyen.ngocquy@team.telstra.com>
2017-12-27 16:44:12 +0800

    Complete simple chat using socket io and js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,19 @@ var express = require("express");
 var app = express();
 var port = 3700;
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
 app.get("/", function(req, res){
-    res.send("It worksr not perfectly!");
+    res.render("page");
 });
 
-app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'Welcome to the chat application' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
 console.log("Listening on port " + port);
